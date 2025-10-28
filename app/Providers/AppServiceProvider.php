34a8f2334a16c4f478253231ee928e142e969ddb
Dejan <dejan_web@outlook.com>
2025-10-28 20:27:21 +0100

    Customize project repository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,35 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useBootstrap();
-        //four categories for footer
-        if (Schema::hasTable('categories')) {
-            $footerCategories = Category::orderBy('created_at', 'desc')
-                ->take(4)
-                ->get();
-            view()->share(compact('footerCategories'));
-        }
-        //three latest project
-        if (Schema::hasTable('projects')) {
-            $latestFooterProjects = Project::standardRequest()
-                ->take(3)
-                ->get();
-            view()->share(compact('latestFooterProjects'));
-        }
-        if (Schema::hasTable('categories')) {
-            $allCategoriesForBlogPartial = Category::withCount(['projects' => function ($query) {
-                $query->orderBy('created_at', 'desc')
-                ->where('enable', 1);
-            }])->get();
-            view()->share(compact('allCategoriesForBlogPartial'));
-        }
-        /*if (Schema::hasTable('tags')) {
-            $allTagsForBlogPartial = Tag::withCount(['projects as enabled_projects_count' => function ($query) {
-                $query->where('enable', true);
-            }])
-                ->orderByDesc('enabled_projects_count')
-                ->get();
 
-            view()->share(compact('allTagsForBlogPartial'));
-        }*/
     }
 }
--- app/Repositories/Front/ProjectRepository.php
+++ app/Repositories/Front/ProjectRepository.php
@@ -10,22 +10,6 @@ class ProjectRepository
     /**
      * index page
      */
-
-    public function getLatestProjects()
-    {
-        $cacheKeyLatest = "latestProjectsSlider";
-        $latestProjectsSlider = Cache::remember($cacheKeyLatest, 1200, function () {
-            return Project::standardRequest()
-                ->limit(12)
-                ->get();
-        });
-        Project::addCacheKeyToIndex($cacheKeyLatest);
-        return $latestProjectsSlider;
-    }
-
-    /**
-     * blog pages
-     */
     public function getBlogProjects()
     {
         $page = request('page', 1);
@@ -36,21 +20,6 @@ class ProjectRepository
         });
         Project::addCacheKeyToIndex($cacheKey);
         return $blogProjects;
-    }
-
-    public function getCategoryProjects($categoryId)
-    {
-        $page = request('page', 1);
-        $cacheKey = "categoryProjects_{$categoryId}_page_{$page}";
-        $categoryProjects = Cache::remember($cacheKey, 400, function () use ($categoryId) {
-            return Project::with('category')
-                ->where('category_id', $categoryId)
-                ->where('enable', 1)
-                ->orderBy('created_at', 'desc')
-                ->paginate(4);
-        });
-        Project::addCacheKeyToIndex($cacheKey);
-        return $categoryProjects;
     }
 
     public function getSingleProject($id, $slug)
@@ -74,45 +43,6 @@ class ProjectRepository
         }
     }
 
-    public function getPrevProject($id, $singleProject)
-    {
-        $cacheKeyPrev = "prevProject_{$id}";
-        $prevProject = Cache::remember($cacheKeyPrev, 300, function () use ($id, $singleProject) {
-            return Project::where('id', '<', $singleProject->id)
-                ->where('enable', 1)
-                ->orderBy('id', 'desc')
-                ->first();
-        });
-        Project::addCacheKeyToIndex($cacheKeyPrev);
-        return $prevProject;
-    }
-
-    public function getNextProject($id, $singleProject)
-    {
-        $cacheKeyNext = "nextProject_{$id}";
-        $nextProject = Cache::remember($cacheKeyNext, 300, function () use ($id, $singleProject) {
-            return Project::where('id', '>', $singleProject->id)
-                ->where('enable', 1)
-                ->orderBy('id', 'asc')
-                ->first();
-        });
-        Project::addCacheKeyToIndex($cacheKeyNext);
-        return $nextProject;
-    }
-    public function getTagProjects($id, $tag)
-    {
-        $page = request('page', 1);
-        $cacheKey = "tagProjects_{$id}_page_{$page}";
-        $tagProjects = Cache::remember($cacheKey, 400, function () use ($tag) {
-            return $tag->projects()
-                ->with('category')
-                ->where('enable', 1)
-                ->orderBy('created_at', 'desc')
-                ->paginate(4);
-        });
-        Project::addCacheKeyToIndex($cacheKey);
-        return $tagProjects;
-    }
     public function getProjectsResult($query)
     {
         $page = request('page', 1);
