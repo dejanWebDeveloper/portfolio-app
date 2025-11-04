90519a894dfe7858468c172c3bd57da526e25139
Dejan <dejan_web@outlook.com>
2025-11-04 20:28:27 +0100

    Change profile photo to links page

--- resources/views/front/links_page/partials/content.blade.php
+++ resources/views/front/links_page/partials/content.blade.php
@@ -1,5 +1,5 @@
 <main class="pb-32 pt-32 flex flex-col items-center px-6">
-    <img src="{{asset('photo/dejan_jovanovic.jpg')}}" alt="Dejan Jovanovic" class="rounded-full w-32 h-32 mb-6 border-4 border-indigo-600 shadow-lg" />
+    <img src="{{asset('photo/profile_photo.jpeg')}}" alt="Dejan Jovanovic" class="rounded-full w-32 h-32 mb-6 border-4 border-indigo-600 shadow-lg" />
     <h1 class="text-3xl font-bold mb-2">Dejan Jovanovic</h1>
     <p class="text-gray-400 mb-8">Junior PHP / Laravel Developer</p>
 
